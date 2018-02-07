a02322319a6b4040bf85bbf9c899e1ffdffbb783
ike <ike@devlaptop.localhost>
2018-02-06 21:54:10 -0500

    modified conf to use yaml syntax, add exception handling to gd2u.py

---
+++ gd2u.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import requests
+import yaml
+
+from sys import exit()
+from time import sleep
+
+def readConfigFile(file):
+    try:
+        with open(file, 'r') as f:
+            configs = yaml.load(f)
+        return configs
+    except:
+        exit("Unable to parse gd2u configuration file")
+
+def getIP():
+    url = 'https://api.ipify.org'
+    try:
+        return requests.get(url).text
+    except:
+        exit("Unable to get current external IP address")
+
+def ipChanged(ip):
+    if ip != getIP():
+        return True
+    else:
+        return False
+
+def updateRecord(domain, username, password):
+    url = "https://{username}:{password}@domains.google.com/nic/update".format(username=username, password=password)
+    data = {"hostname" : domain, "myip" : self.ip}
+    try:
+        requests.post(url, data=data)
+    except:
+        exit("Unable to update Google Domains Dynamic DNS Record")
+
+def run():
+    currentIP = '0.0.0.0'
+    configs = readConfigFile('/opt/gd2u/gd2u.conf')
+
+    while True:
+        if ipChanged(currentIP):
+            for domain in configs.get('domains'):
+                updateRecord(domain.get('subdomain'), domain.get('username'), domain.get('password'))
+        sleep(configs.get('global'.get('check_interval')))
+
+if __name__ == "__main__":
+    run()
